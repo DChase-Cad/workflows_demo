1040f7b0ef789e6975ef5e25c367a884bd321426
DChase-Cad <79270522+DChase-Cad@users.noreply.github.com>
2021-11-11 13:11:13 -0600

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,10 +1,10 @@
-'''github actions intro'''
-
+import sys
 
 def main():
-    '''main'''
-    print("Hello World!")
-
+    print("Hello World! From Python: " + str(sys.version_info))
+    if sys.version_info >= (3, 6) and sys.version_info < (3, 7):
+        # let's make this script fail for Python 3.6
+        raise Exception('Python version 3.6.x is unsupported!')
 
 if __name__ == "__main__":
     main()
